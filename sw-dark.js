2e7d6a3a4aecf36e038c7f52bf4741a35c7f26d1
soso44-blip <welcome.greenbox@gmail.com>
2026-04-29 10:29:36 +0800

    Update asset reference in sw-dark.js

--- sw-dark.js
+++ sw-dark.js
@@ -1,6 +1,6 @@
 const CACHE = 'adhd-dark-v1';
 const ASSETS = [
-  './adhd-dark-final.html',
+  './adhd-home-manager-dark.html',
   'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Nunito:wght@400;600;700;800;900&display=swap',
   'https://fonts.gstatic.com/s/fredokaone/v14/k3kUo8kEI-tA1RRcTZGmTlHGCaen8wf-.woff2',
 ];
